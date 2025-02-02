ec7c351defb45cfd030577d79ea906ed8fe1040a
Nikhil Sirsat <sirsatnikhil617@gmail.com>
2025-02-02 17:07:13 +0530

    Remove Vite Config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api/auth': 'https://wizarding-library-1.onrender.com',
+    server: {
+        // proxy: {
+        //   '/api/auth': 'https://wizarding-library-1.onrender.com',
+        // },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
 });
